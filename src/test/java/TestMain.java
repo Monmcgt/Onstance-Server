4bc12fdfd1d8908d2ee99b37736809ef531977d1
Monmcgt <raninmon2015@gmail.com>
2022-08-30 01:17:32 +0700

    Fix exception handling

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -2,7 +2,12 @@ import me.monmcgt.code.onstance.server.OnstanceServer;
 
 public class TestMain {
     public static void main(String[] args) {
-        OnstanceServer onstanceServer = new OnstanceServer();
+        OnstanceServer onstanceServer;
+        if (args.length == 0) {
+            onstanceServer = new OnstanceServer();
+        } else {
+            onstanceServer = new OnstanceServer(Integer.parseInt(args[0]));
+        }
         onstanceServer.start();
     }
 }
